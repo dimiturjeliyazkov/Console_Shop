7320a2b8fb34d6214d997b296732ac112a5a8c4e
mitkodozer123 <mitkodozer222@abv.bg>
2019-02-18 23:21:09 +0200

    idk

--- Online_Shop/Program.cs
+++ Online_Shop/Program.cs
@@ -11,6 +11,7 @@ namespace Online_Shop
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("hiii");
         }
     }
 }
